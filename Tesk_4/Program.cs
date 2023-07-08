eb5771037d9e9f708d00de9ece7a2931e9cee5fc
IraRybakova67 <rda-67@mail.ru>
2023-07-08 21:49:10 +0300

    Четное число

---
+++ Tesk_4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+max = num1;
+if (num2 > max)
+{
+max = num2;
+}
+else if (num3 > max)
+    {
+    max = nam3;
+    }
+    else
+    {
+    Console.Write($" Максимальным числом является {max}");
+    }
+
+//Console.Write($" Максимальным числом является {max}");
+  
+
+
+
+
+
